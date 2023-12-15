6f575f750d743e9b812e9e7a2b1ac872bc420348
Guilherme Souza <guilhermesouzae21@gmail.com>
2023-12-15 20:08:27 -0300

    feat: generate report

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const token =
   ""; //Token that you saved in step 5 of this tutorial
-import { Client, IntentsBitField, GatewayIntentBits } from "discord.js";
+const connectionString =
+  "";
+
+import { Client, GatewayIntentBits, IntentsBitField } from "discord.js";
 
 const myIntents = new IntentsBitField();
 
 myIntents.add(
   IntentsBitField.Flags.Guilds,
   IntentsBitField.Flags.GuildMessages,
+  IntentsBitField.Flags.GuildIntegrations,
   IntentsBitField.Flags.GuildMessageReactions,
   GatewayIntentBits.MessageContent,
-  GatewayIntentBits.GuildVoiceStates
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.Guilds
 );
 
 import { MongoClient } from "mongodb"; // hFuBTb5y5nZBxj5b
 
-const connectionString =
-  "";
 
 const mongoClient = new MongoClient(connectionString);
 
@@ -26,10 +29,38 @@ const main = async () => {
     console.log("The UptimeBot  is online"); //message when bot is online
   });
 
-  client.on("messageCreate", (msg) => {
-    console.log(msg.content);
-    if (msg.content === "ping") {
-      msg.reply("pong");
+  client.on("messageCreate", async (msg) => {
+    if (msg.content[0] === "/") {
+      if (msg.content === "/ping") {
+        msg.reply("pong");
+      }
+
+      if (msg.content === "/uptime") {
+        try {
+          await mongoClient.connect();
+
+          const database = mongoClient.db("uptime");
+          const uptimeDb = database.collection("uptime-data");
+
+          const users = await uptimeDb.find({ serverId: msg.guildId })
+            .sort({ totalMinutesOnline: -1 })
+            .toArray();
+
+          let text = `Tempo dos usuários no servidor: ${msg.guild.name}\n`;
+
+          users.forEach((u, index) => {
+            text += `${index + 1}° - @${u.userName} ${u.totalMinutesOnline} minutos.\n`;
+          });
+
+          msg.reply(text);
+          return;
+        } catch (e) {
+          console.log(e);
+        } finally {
+          mongoClient.close();
+
+        }
+      }
     }
   });
 
@@ -44,25 +75,65 @@ const main = async () => {
 
         const database = mongoClient.db("uptime");
         const uptimeDb = database.collection("uptime-data");
-        
+
         const user = await uptimeDb.findOne({
           user: newState.member.user.tag,
+          serverId: newState.guild.id
         });
 
-        if(user.totalMinutesOnline){
-          const differenceInMilliseconds = new Date() - user.lastTimeEntered;
-          const minutesOnline = Math.floor(differenceInMilliseconds / 1000 / 60);
+        // first time entered
+        if (user == null) {
+          await uptimeDb.insertOne({
+            user: newState.member.user.tag,
+            totalMinutesOnline: 0,
+            lastTimeEntered: new Date(),
+            serverId: newState.guild.id,
+            userName: newState.member.user.displayName
+          });
+          return;
         }
-        console.log('user before', user);
-        const result = await uptimeDb.insertOne({
-          user: newState.member.user.tag,
-          totalMinutesOnline:  user.totalMinutesOnline ? user.totalMinutesOnline : 0,
-          lastTimeEntered: new Date()
+
+        // already exists
+        await uptimeDb.updateOne({ _id: user._id, serverId: newState.guild.id }, {
+          $set: {
+            lastTimeEntered: new Date()
+          }
         });
-        console.log('user after', user);
-        console.log(
-          `A document was inserted with the _id: ${result.insertedId}`
-        );
+
+      } catch (e) {
+        console.log(e);
+      } finally {
+        mongoClient.close();
+      }
+    }
+    if (oldState.channelId && !newState.channelId) {
+      console.log(
+        `User ${oldState.member.user.tag} left voice channel ${oldState.channel.name}`
+      );
+      try {
+        await mongoClient.connect();
+
+        const database = mongoClient.db("uptime");
+        const uptimeDb = database.collection("uptime-data");
+
+        const user = await uptimeDb.findOne({
+          user: oldState.member.user.tag,
+          serverId: oldState.guild.id
+        });
+
+        if (!user) return;
+
+        const differenceInMilliseconds = new Date() - user.lastTimeEntered;
+        const minutesOnline = Math.ceil(differenceInMilliseconds / 1000 / 60);
+
+        const updateQuery = { _id: user._id, serverId: oldState.guild.id };
+        const newValues = {
+          $set: {
+            totalMinutesOnline: user.totalMinutesOnline + minutesOnline
+          }
+        };
+
+        await uptimeDb.updateOne(updateQuery, newValues);
       } catch (e) {
         console.log(e);
       } finally {
@@ -71,7 +142,7 @@ const main = async () => {
     }
   });
 
-    client.login(token);
+  client.login(token);
 };
 
 main().catch(console.error);
